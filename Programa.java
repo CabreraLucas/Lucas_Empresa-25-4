01bc2aaf4c98380142c3c3c90aee83e9c8f3fb65
CabreraLucas <lucas.cabrera@fatec.sp.gov.br>
2022-04-25 19:28:19 +0000

    Empresa

--- Programa.java
+++ Programa.java
@@ -7,52 +7,59 @@ public class Programa {
 		v.setEstaNaEmpresa(true);
 		v.setNome("Eduardo");
 		v.setRg("45");
+        v.setSalario(1400.0);
         
         Gerente g1 = new Gerente();
-        g1.departamento = "Vendas";
-        g1.dataDeEntrada = "03/02/1990";
-        g1.estaNaEmpresa = true;
-        g1.nome = "Eric";
-        g1.rg = "48";
-        g1.salario = 3500.0;
+        g1.setDepartamento("Gerencia");
+        g1.setDataDeEntrada("03/02/1990");
+        g1.setEstaNaEmpresa(true);
+        g1.setNome("Eric");
+        g1.setRg("48");
+        g1.setSalario(4000.0);
 
         g1.setLogin("eric");
         g1.setSenha("123");
 
         Supervisor supervisor = new Supervisor();
-        supervisor.nome = "Julio";
-        supervisor.dataDeEntrada = "10/10/2010";
-        supervisor.salario = 5000.0;
+        supervisor.setNome("Julio");
+        supervisor.setDataDeEntrada("10/10/2010");
+        supervisor.setSalario(3000.0);
+        supervisor.setRg("50");
+        supervisor.setDepartamento("Gerência");
+        supervisor.setEstaNaEmpresa(true);
+
 
         ControleDeBonificacao controle = new ControleDeBonificacao();
         controle.calculaTotalDeBonus(v);
         controle.calculaTotalDeBonus(g1);
         controle.calculaTotalDeBonus(supervisor);
 
+        System.out.println("Nome: " +v.getNome());
         System.out.println("Data de admissão: " +v.getDataDeEntrada());
 		System.out.println("Departamento: " +v.getDepartamento());
-		System.out.println("Nome: " +v.getNome());
 		System.out.println("RG: "+v.getRg());
 		System.out.println("Salário: "+v.getSalario());
-		System.out.println("Contratado: "+v.isEstaNaEmpresa());
+		//System.out.println("Contratado: "+v.isEstaNaEmpresa());
 		System.out.println("Bonificação: "+v.bonifica());
 
 
         System.out.println("//////////////////////////////");
 		g1.autentica("eric", "123");
-		System.out.println("Data de admissão: " +g1.getDataDeEntrada());
-		System.out.println("Departamento: " +g1.getDepartamento());
 		System.out.println("Nome: " +g1.getNome());
+        System.out.println("Data de admissão: " +g1.getDataDeEntrada());
+		System.out.println("Departamento: " +g1.getDepartamento());
 		System.out.println("RG: " +g1.getRg());
 		System.out.println("Salário: " +g1.getSalario());		
 		System.out.println("Bonificação: " +g1.bonifica());
-		System.out.println("Total de bonus: R$" + controle.getTotalDeBonus());
+		//System.out.println("Total de bonus: R$" + controle.getTotalDeBonus());
     
         System.out.println("//////////////////////////////");
         System.out.println("Nome:" +supervisor.getNome());
         System.out.println("Data de admissão: " +supervisor.getDataDeEntrada());
+        System.out.println("Departamento: "+supervisor.getDepartamento());
+        System.out.println(("RG: " +supervisor.getRg()));
         System.out.println("Salário: " +supervisor.getSalario());
-
+        System.out.println("Bonificação: "+supervisor.bonifica());
     }
-
+    
 }
